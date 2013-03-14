dbd860b078a0798135621793c383c74b83f61620
Teemu Törrö <teemu.torro@gmail.com>
2013-03-14 22:47:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testisofta::Application.config.secret_token = '1ca9a23e522d961f310e0802c9bf4bfb4f012496ad830c2b1cd77a238d4131df2547a5c73637babbbf8222c52b7fd3f273c61ba17422bc2136506c8f0fc655bf'
